6ae68b94fd88b54fce8c487c85d2e8e8b853d4d8
Felipe Rocha <felipergomes19@gmail.com>
2025-03-31 09:02:54 -0300

    Adiciona os arquivos 'Exercício_5', 'Exercício_6', 'Exerćício_7' e 'Exercício_8'

---
+++ Java/Laços_Repetição/Lista While/Exercício_8.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author CAMARGO
+ */
+import java.util.Scanner;
+public class Exercício_8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        int i = 0;
+        Scanner Entrada = new Scanner(System.in);
+        System.out.println("Informe a quantidade de lados,entre 1 e 20");
+        int Lados = Entrada.nextInt();
+        if(Lados < 1|| Lados > 20)
+        {
+            System.out.println("NUMERO INVALIDO, REINICIE O CODIGO");
+        }
+        else
+        {
+            while(i < Lados)
+            {
+                int j = 0;
+                while(j < Lados)
+                {
+                    j++;
+                    if(i == 0|| i == Lados - 1|| j == 0 || j == Lados)
+                    {
+                        System.out.println(" * ");
+                    }
+                    else
+                    {
+                       System.out.println("  ");
+                    } 
+                    
+                }
+            }
+        }
+
+    }
+}
