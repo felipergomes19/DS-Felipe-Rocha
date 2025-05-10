ae3edcbf6199d1e87d0df05bd442b56e2fc34772
Felipe Rocha <felipergomes19@gmail.com>
2025-05-10 17:05:13 -0300

    Atualiza o arquivo 'Exercício_1.java' da pasta 'POO'

--- Java/POO/Exercício_1.java
+++ Java/POO/Exercício_1.java
@@ -1,10 +1,10 @@
 
     // Superclasee
 class Veiculo{
-        // Atributos comuns
-    String marca;
-    String modelo;
-    int ano; 
+        // Atributos privados (encapsulados)
+    private String marca;
+    private String modelo;
+    private int ano; 
     
     
         // Construtor da classe Veiculo
@@ -13,6 +13,18 @@ class Veiculo{
         this.modelo = modelo;
         this.ano = ano;
 }
+     // Métodos getters
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
         // Metodo para exibir as informações basicas
     public void ExibirInformacao(){
     System.out.println("Marca = "+marca);
@@ -22,20 +34,48 @@ class Veiculo{
 }
     // Subclasse que herda da superclasse (Veiculo)
 class Carro extends Veiculo{
-        // Atributos especifico do Carro
-    int portas;
-    int rodas;
-    String cor;
+    
+        // Atributos privados específicos da subclasse
+    private int portas;
+    private int rodas;
+    private String cor;
+    
         // Construtor da classe Carro
     public Carro(String marca,String modelo, int ano, int portas, int rodas, String cor){
+        
         // Usando o super para chamar o construtor da superclasse
         super(marca, modelo, ano);
         this.portas = portas;
         this.rodas = rodas;
         this.cor = cor;
     }
+    // Getters e Setters
+    public int getPortas() {
+        return portas;
+    }
+
+    public void setPortas(int portas) {
+        this.portas = portas;
+    }
+
+    public int getRodas() {
+        return rodas;
+    }
+
+    public void setRodas(int rodas) {
+        this.rodas = rodas;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
              // Sobrescrevendo o método da superclasse para adicionar mais informações
         public void ExibirInformacao(){
+            
             // Chama o método da superclasse
             super.ExibirInformacao();
             System.out.println("Numero de portas = "+portas);
